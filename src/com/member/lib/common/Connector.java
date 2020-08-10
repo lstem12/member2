b2845c3aa8d152c2e24dbefa78d67be8930e3872
Host <Host@DESKTOP-L9VLQI0>
2020-08-10 21:44:44 +0900

    1

--- src/com/member/lib/common/Connector.java
+++ src/com/member/lib/common/Connector.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class Connector {
 
 	private static final String URL ="jdbc:oracle:thin:@localhost:1521/xe";
-	private static final String ID = "c##test";
+	private static final String ID = "test";
 	private static final String PWD = "test";
 	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
 	
